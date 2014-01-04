6b7e4c8d2e1622f2989fac73e958fe8ac7344f8e
Allan Hortle <allanhortle@gmail.com>
2014-01-04 11:43:08 +1100

    grunting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,61 @@
+
+/*
+|--------------------------------------------------------------------------
+| Centurion Grunt File
+|--------------------------------------------------------------------------
+|
+| If you would like to edit the color of your Centurion file just change
+| the color of 'config.hero'. There are some predefined colors here
+| already but feel free to just throw your own one in.
+|
+*/
+
 module.exports = function(grunt) {
 	// Load tasks
 	require('load-grunt-tasks')(grunt);
 
 	var config = {
 
-		// Colors
+		// Colors used in the theme
 		green:[96,172,123],
 		blue:[90, 138, 165],
-		yellow:[218, 174, 105],
-		orange:[225, 134, 84],
 		red:[190, 90, 78],
 
-		// Paths
+		// Extra colors from Galea
+		yellow:[218, 174, 105],
+		orange:[225, 134, 84],
+
+		/*
+		|--------------------------------------------------------------------------
+		| Theme Folder Name
+		|--------------------------------------------------------------------------
+		|
+		| If you clone centurion into a separate folder, just change this variable
+		| to match. Sublime text requires a few hard-coded file paths. If this
+		| doesn't match you won't get icons and your text boxes will go white.
+		|
+		*/
+
 		folderName:'Theme - Centurion'
 
 	}
 
+
+	/*
+	|--------------------------------------------------------------------------
+	| SET THE THEME COLOR HERE!
+	|--------------------------------------------------------------------------
+	*/
+
+	config.hero = config.green;
+
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Don't worry about below here. Just the Grunt Stuff
+	|--------------------------------------------------------------------------
+	*/
 
 	// Color Functions
 	var colorChange = function(arr, value){
@@ -26,15 +66,12 @@ module.exports = function(grunt) {
 		return a;
 	}
 
-	// Set theme Color here
-	config.hero = config.green;
-
 	//Define Color variations
 	config.heroDarker1 = colorChange(config.hero,-32);
 	config.heroDarker2 = colorChange(config.hero,-64);
 	config.heroLighter1 = colorChange(config.hero,32);
 
-	// Project configuration.
+	// Grunt Config
 	grunt.initConfig({
 		concat: {
 			options: {
@@ -78,7 +115,6 @@ module.exports = function(grunt) {
 		},
 	});
 
-	// Default task.
 	grunt.registerTask('default', [
 		'concat',
 		'string-replace'
