e50fb29b943016fa774ca380220a4276ca5e7d89
Allan Hortle <allanhortle@gmail.com>
2014-08-29 22:00:07 +1000

    colors

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,62 +16,17 @@ module.exports = function(grunt) {
 
 	var config = {
 
-		// Colors used in the theme
 		green:[96,172,123],
-		blue:[108,166,206],
-		
-		red:[190, 90, 78],
-
-		// Extra colors from Galea
-		yellow:[218, 174, 105],
-		orange:[224,133,90],
-
-		/*
-		|--------------------------------------------------------------------------
-		| Theme Folder Name
-		|--------------------------------------------------------------------------
-		|
-		| If you clone centurion into a separate folder, just change this variable
-		| to match. Sublime text requires a few hard-coded file paths. If this
-		| doesn't match you won't get icons and your text boxes will go white.
-		|
-		*/
+		blue:[108,166,206],		
+		red:[197,91,78],
+		yellow:[225,174,105],
+		orange:[225,134,90],
 
 		folderName:'Theme - Centurion'
 
 	}
 
-
-	/*
-	|--------------------------------------------------------------------------
-	| SET THE THEME COLOR HERE!
-	|--------------------------------------------------------------------------
-	*/
-
 	config.hero = config.green;
-
-
-
-	/*
-	|--------------------------------------------------------------------------
-	| Don't worry about below here. Just the Grunt Stuff
-	|--------------------------------------------------------------------------
-	*/
-
-	// Color Functions
-	var colorChange = function(arr, value){
-		var a = arr.slice();
-		for (var i = a.length - 1; i >= 0; i--) {
-			a[i] = a[i] + (value);
-		};
-		return a;
-	}
-
-	//Define Color variations
-	config.heroDarker1 = colorChange(config.hero,-32);
-	config.heroDarker2 = colorChange(config.hero,-64);
-	config.heroLighter1 = colorChange(config.hero,32);
-
 
 	// Grunt Config
 	grunt.initConfig({
